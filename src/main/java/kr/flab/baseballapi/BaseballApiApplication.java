9ea59a28d48f62ec8b2f1ae66045fa355f037a8f
Deepred <deepred.kim@gmail.com>
2022-08-01 10:06:34 +0900

    feat: Spring Initializr
    Gradle, SpringBoot 2.7.2, Jar, 11, Spring Web/JPA/H2/Lombok

---
+++ src/main/java/kr/flab/baseballapi/BaseballApiApplication.java
@@ -0,0 +1,13 @@
+package kr.flab.baseballapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/flab/baseballapi/BaseballApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.flab.baseballapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
